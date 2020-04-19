5be71369b4366442a3690c0c879aced529809186
Amit Bansal <007amitbansal@gmail.com>
2020-04-19 14:29:45 +0530

    move election logic to election.go

---
+++ src/raft/election.go
@@ -0,0 +1,176 @@
+package raft
+
+import "../labrpc"
+import "time"
+import crand "crypto/rand"
+import "math/big"
+
+func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
+	// DPrintf("Sending RequestVote to %d", server)
+	ok := rf.peers[server].Call("Raft.RequestVoteRpc", args, reply)
+	return ok
+}
+
+func (rf *Raft) SendHeartBeats() {
+	for {
+		rf.mu.Lock()
+		if rf.killed() {
+			rf.mu.Unlock()
+			return
+		}
+		for rf.currentState != LEADER {
+			rf.rfCond.Wait()
+		}
+		DPrintln("[", rf.me, "]", "Sending HeartBeats for term =>", rf.currentTerm)
+		appendEntriesArgs := AppendEntriesArgs{
+			Term:     rf.currentTerm,
+			LeaderId: rf.me,
+			// PreLogIndex  int
+			// PrevLogTerm  int
+			// Entries      []int
+			// LeaderCommit int
+		}
+		for _, clientEnd := range rf.peers {
+			reply := AppendEntriesReply{}
+			go func(clientEnd *labrpc.ClientEnd, args *AppendEntriesArgs, reply *AppendEntriesReply) {
+				clientEnd.Call("Raft.AppendEntries", args, reply)
+				rf.mu.Lock()
+				if reply.Term > rf.currentTerm {
+					rf.currentTerm = reply.Term
+					rf.currentState = FOLLOWER
+				}
+				rf.mu.Unlock()
+
+			}(clientEnd, &appendEntriesArgs, &reply)
+		}
+
+		rf.mu.Unlock()
+		time.Sleep(250 * time.Millisecond)
+	}
+}
+
+func (rf *Raft) ConductElection() {
+	rf.mu.Lock()
+	if rf.killed() {
+		rf.mu.Unlock()
+		return
+	}
+	rf.IncTerm()
+	DPrintln("[", rf.me, "]", "Election started by %d", rf.me, "for term =>", rf.currentTerm)
+	rf.currentState = CANDIDATE
+	rf.votedFor = rf.me
+
+	requestVoteArgs := &RequestVoteArgs{
+		Term:        rf.currentTerm,
+		CandidateId: rf.me,
+	}
+
+	requestVoteReplyMp := make(map[int]*RequestVoteReply)
+
+	requestVoteReplyMp[rf.me] = &RequestVoteReply{
+		Term:        rf.currentTerm,
+		VoteGranted: true,
+	}
+	var repliesCount int
+	repliesCount = 0
+	for i, _ := range rf.peers {
+		if i != rf.me {
+			reply := &RequestVoteReply{}
+			requestVoteReplyMp[i] = reply
+			go func(i int, args *RequestVoteArgs, reply *RequestVoteReply, repliesCount *int) {
+				ok := rf.sendRequestVote(i, requestVoteArgs, reply)
+				rf.mu.Lock()
+				if ok {
+					*repliesCount = *repliesCount + 1
+				}
+				rf.mu.Unlock()
+			}(i, requestVoteArgs, reply, &repliesCount)
+		}
+	}
+
+	rf.mu.Unlock()
+	//Wait for votes
+	time.Sleep(time.Millisecond * 60)
+	rf.mu.Lock()
+	DPrintln("[", rf.me, "]", "Counting votes for ", rf.me, "Replies count", repliesCount, "peers => ", len(rf.peers)/2)
+	if repliesCount >= (len(rf.peers) / 2) {
+		votes := 0
+		for i, reply := range requestVoteReplyMp {
+			DPrintln("[", rf.me, "]", "Reply from ", i, reply)
+
+			if reply.Term > rf.currentTerm {
+				rf.currentState = FOLLOWER
+				rf.currentTerm = reply.Term
+				rf.votedFor = -1
+				rf.rfCond.Broadcast()
+				rf.mu.Unlock()
+				return
+			} else {
+				if reply.VoteGranted {
+					votes += 1
+				}
+			}
+		}
+		if votes > (len(rf.peers) / 2) {
+			rf.currentState = LEADER
+			DPrintln("[", rf.me, "]", "LEADER is here....", rf.me)
+			rf.rfCond.Broadcast()
+		}
+	}
+	rf.mu.Unlock()
+}
+
+func (rf *Raft) StartElectionProcess() {
+	for {
+		rf.mu.Lock()
+
+		if rf.killed() {
+			rf.mu.Unlock()
+			return
+		}
+
+		if rf.currentState == CANDIDATE {
+			rf.mu.Unlock()
+			rf.ConductElection()
+		} else {
+			rf.votedFor = -1
+			rf.mu.Unlock()
+			return
+		}
+
+		max := big.NewInt(1000)
+		rr, _ := crand.Int(crand.Reader, max)
+		DPrintln("[", rf.me, "]", "Election paused for ", rr.Int64())
+		time.Sleep(time.Duration(rr.Int64()) * time.Millisecond)
+
+	}
+}
+
+//if communication is not received for LastRpcTimeOut calls for an election
+func (rf *Raft) CheckAndKickOfLeaderElection() {
+	for {
+		max := big.NewInt(200)
+		rr, _ := crand.Int(crand.Reader, max)
+		time.Sleep(time.Duration(rr.Int64()) * time.Millisecond)
+
+		rf.mu.Lock()
+		if rf.killed() {
+			rf.mu.Unlock()
+			return
+		}
+
+		timeOutAt := rf.lastRpcReceived.Add(LastRpcTimeOut)
+		if time.Now().After(timeOutAt) {
+			rf.currentState = CANDIDATE
+			go rf.StartElectionProcess()
+
+			rf.mu.Unlock()
+			// let the election happen
+			time.Sleep(2000 * time.Millisecond)
+
+		} else {
+			rf.mu.Unlock()
+			time.Sleep(timeOutAt.Sub(time.Now()))
+		}
+	}
+}
--- src/raft/raft.go
+++ src/raft/raft.go
@@ -21,9 +21,9 @@ import "sync"
 import "sync/atomic"
 import "../labrpc"
 import "time"
-import crand "crypto/rand"
-import "math/big"
 
+// import crand "crypto/rand"
+// import "math/big"
 
 // import "bytes"
 // import "../labgob"
@@ -208,41 +208,6 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 }
 
 //
-// example code to send a RequestVote RPC to a server.
-// server is the index of the target server in rf.peers[].
-// expects RPC arguments in args.
-// fills in *reply with RPC reply, so caller should
-// pass &reply.
-// the types of the args and reply passed to Call() must be
-// the same as the types of the arguments declared in the
-// handler function (including whether they are pointers).
-//
-// The labrpc package simulates a lossy network, in which servers
-// may be unreachable, and in which requests and replies may be lost.
-// Call() sends a request and waits for a reply. If a reply arrives
-// within a timeout interval, Call() returns true; otherwise
-// Call() returns false. Thus Call() may not return for a while.
-// A false return can be caused by a dead server, a live server that
-// can't be reached, a lost request, or a lost reply.
-//
-// Call() is guaranteed to return (perhaps after a delay) *except* if the
-// handler function on the server side does not return.  Thus there
-// is no need to implement your own timeouts around Call().
-//
-// look at the comments in ../labrpc/labrpc.go for more details.
-//
-// if you're having trouble getting RPC to work, check that you've
-// capitalized all field names in structs passed over RPC, and
-// that the caller passes the address of the reply struct with &, not
-// the struct itself.
-//
-func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
-	// DPrintf("Sending RequestVote to %d", server)
-	ok := rf.peers[server].Call("Raft.RequestVoteRpc", args, reply)
-	return ok
-}
-
-//
 // the service using Raft (e.g. a k/v server) wants to start
 // agreement on the next command to be appended to Raft's log. if this
 // server isn't the leader, returns false. otherwise start the
@@ -266,17 +231,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	return index, term, isLeader
 }
 
-//
-// the tester doesn't halt goroutines created by Raft after each test,
-// but it does call the Kill() method. your code can use killed() to
-// check whether Kill() has been called. the use of atomic avoids the
-// need for a lock.
-//
-// the issue is that long-running goroutines use memory and may chew
-// up CPU time, perhaps causing later tests to fail and generating
-// confusing debug output. any goroutine with a long-running loop
-// should call killed() to check whether it should stop.
-//
 func (rf *Raft) Kill() {
 	atomic.StoreInt32(&rf.dead, 1)
 	DPrintln("[", rf.me, "]", "Kill called....")
@@ -287,170 +241,6 @@ func (rf *Raft) killed() bool {
 	DPrintln("[", rf.me, "]", "Killed called....")
 	z := atomic.LoadInt32(&rf.dead)
 	return z == 1
-}
-
-func (rf *Raft) SendHeartBeats() {
-	for {
-		rf.mu.Lock()
-		if rf.killed() {
-			rf.mu.Unlock()
-			return
-		}
-		for rf.currentState != LEADER {
-			rf.rfCond.Wait()
-		}
-		DPrintln("[", rf.me, "]", "Sending HeartBeats for term =>", rf.currentTerm)
-		appendEntriesArgs := AppendEntriesArgs{
-			Term:     rf.currentTerm,
-			LeaderId: rf.me,
-			// PreLogIndex  int
-			// PrevLogTerm  int
-			// Entries      []int
-			// LeaderCommit int
-		}
-		for _, clientEnd := range rf.peers {
-			reply := AppendEntriesReply{}
-			go func(clientEnd *labrpc.ClientEnd, args *AppendEntriesArgs, reply *AppendEntriesReply) {
-				clientEnd.Call("Raft.AppendEntries", args, reply)
-				rf.mu.Lock()
-				if reply.Term > rf.currentTerm {
-					rf.currentTerm = reply.Term
-					rf.currentState = FOLLOWER
-				}
-				rf.mu.Unlock()
-
-			}(clientEnd, &appendEntriesArgs, &reply)
-		}
-
-		rf.mu.Unlock()
-		time.Sleep(250 * time.Millisecond)
-	}
-}
-
-func (rf *Raft) ConductElection() {
-	rf.mu.Lock()
-	if rf.killed() {
-		rf.mu.Unlock()
-		return
-	}
-	rf.IncTerm()
-	DPrintln("[", rf.me, "]", "Election started by %d", rf.me, "for term =>", rf.currentTerm)
-	rf.currentState = CANDIDATE
-	rf.votedFor = rf.me
-
-	requestVoteArgs := &RequestVoteArgs{
-		Term:        rf.currentTerm,
-		CandidateId: rf.me,
-	}
-
-	requestVoteReplyMp := make(map[int]*RequestVoteReply)
-
-	requestVoteReplyMp[rf.me] = &RequestVoteReply{
-		Term:        rf.currentTerm,
-		VoteGranted: true,
-	}
-	var repliesCount int
-	repliesCount = 0
-	for i, _ := range rf.peers {
-		if i != rf.me {
-			reply := &RequestVoteReply{}
-			requestVoteReplyMp[i] = reply
-			go func(i int, args *RequestVoteArgs, reply *RequestVoteReply, repliesCount *int) {
-				ok := rf.sendRequestVote(i, requestVoteArgs, reply)
-				rf.mu.Lock()
-				if ok {
-					*repliesCount = *repliesCount + 1
-				}
-				rf.mu.Unlock()
-			}(i, requestVoteArgs, reply, &repliesCount)
-		}
-	}
-
-	rf.mu.Unlock()
-	//Wait for votes
-	time.Sleep(time.Millisecond * 60)
-	rf.mu.Lock()
-	DPrintln("[", rf.me, "]", "Counting votes for ", rf.me, "Replies count", repliesCount, "peers => ", len(rf.peers)/2)
-	if repliesCount >= (len(rf.peers) / 2) {
-		votes := 0
-		for i, reply := range requestVoteReplyMp {
-			DPrintln("[", rf.me, "]", "Reply from ", i, reply)
-
-			if reply.Term > rf.currentTerm {
-				rf.currentState = FOLLOWER
-				rf.currentTerm = reply.Term
-				rf.votedFor = -1
-				rf.rfCond.Broadcast()
-				rf.mu.Unlock()
-				return
-			} else {
-				if reply.VoteGranted {
-					votes += 1
-				}
-			}
-		}
-		if votes > (len(rf.peers) / 2) {
-			rf.currentState = LEADER
-			DPrintln("[", rf.me, "]", "LEADER is here....", rf.me)
-			rf.rfCond.Broadcast()
-		}
-	}
-	rf.mu.Unlock()
-}
-
-func (rf *Raft) StartElectionProcess() {
-	for {
-		rf.mu.Lock()
-
-		if rf.killed() {
-			rf.mu.Unlock()
-			return
-		}
-
-		if rf.currentState == CANDIDATE {
-			rf.mu.Unlock()
-			rf.ConductElection()
-		} else {
-			rf.votedFor = -1
-			rf.mu.Unlock()
-			return
-		}
-
-		max := big.NewInt(1000)
-		rr, _ := crand.Int(crand.Reader, max)
-		DPrintln("[", rf.me, "]", "Election paused for ", rr.Int64())
-		time.Sleep(time.Duration(rr.Int64()) * time.Millisecond)
-
-	}
-}
-
-//if communication is not received for LastRpcTimeOut calls for an election
-func (rf *Raft) CheckAndKickOfLeaderElection() {
-	for {
-		max := big.NewInt(200)
-		rr, _ := crand.Int(crand.Reader, max)
-		time.Sleep(time.Duration(rr.Int64()) * time.Millisecond)
-
-		rf.mu.Lock()
-		if rf.killed() {
-			rf.mu.Unlock()
-			return
-		}
-
-		timeOutAt := rf.lastRpcReceived.Add(LastRpcTimeOut)
-		if time.Now().After(timeOutAt) {
-			rf.currentState = CANDIDATE
-			go rf.StartElectionProcess()
-
-			rf.mu.Unlock()
-			// let the election happen
-			time.Sleep(2000 * time.Millisecond)
-
-		} else {
-			rf.mu.Unlock()
-			time.Sleep(timeOutAt.Sub(time.Now()))
-		}
-	}
 }
 
 //
