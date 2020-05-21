9212b3613c12a5e3c9b20528669a615c50b38a82
Amit Bansal <007amitbansal@gmail.com>
2020-05-21 21:36:19 +0530

    Pass 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,10 +3,13 @@ package kvraft
 import "../labrpc"
 import "crypto/rand"
 import "math/big"
-
+import "fmt"
+import "time"
 
 type Clerk struct {
-	servers []*labrpc.ClientEnd
+	servers  []*labrpc.ClientEnd
+	leaderId int
+	id       int64
 	// You will have to modify this struct.
 }
 
@@ -20,6 +23,7 @@ func nrand() int64 {
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
+	ck.id = nrand()
 	// You'll have to add code here.
 	return ck
 }
@@ -38,8 +42,56 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 //
 func (ck *Clerk) Get(key string) string {
 
-	// You will have to modify this function.
-	return ""
+	args := GetArgs{
+		Key:      key,
+		ReqId:    nrand(),
+		ClientId: ck.id,
+	}
+
+	leaderId := ck.leaderId
+	timeoutTtries := 0
+	wrongLeader := 0
+	
+	for {
+		reply := GetReply{}
+		if timeoutTtries > 50 {
+			// fmt.Println("KVServer.Get try limit exceeded", args)
+			// return ""
+		}
+		if wrongLeader % len(ck.servers) == 0 &&  wrongLeader > 100 {
+			// fmt.Println("KVServer.Get try limit exceeded, couldn't connect to a leader", args)
+			// return ""
+		}
+
+		if ok := ck.servers[leaderId].Call("KVServer.Get", &args, &reply); ok {
+			switch reply.Err {
+			case OK:
+				ck.leaderId = leaderId
+				return reply.Value
+			case ErrNoKey:
+				return ""
+			case ErrWrongLeader:
+				// fmt.Println("KVServer.Get ErrWrongLeader trying again", args)
+				wrongLeader += 1
+				if wrongLeader % len(ck.servers) == 0 {
+					time.Sleep(100 * time.Millisecond)
+				}
+			case ErrTimeout:
+				// fmt.Println("KVServer.Get ErrTimeout trying again", args)
+				timeoutTtries += 1
+				time.Sleep(100 * time.Millisecond)
+			default:
+				fmt.Println("Get Unknown", reply.Err)
+			}
+		} else {
+			time.Sleep(100 * time.Millisecond)
+			timeoutTtries += 1
+			// fmt.Println("KVServer.Get req not ok", args)
+		}
+		leaderId = (leaderId + 1) % len(ck.servers)
+	}
+
+	// return ""
 }
 
 //
@@ -53,7 +105,54 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:      key,
+		Value:    value,
+		Op:       op,
+		ReqId:    nrand(),
+		ClientId: ck.id,
+	}
+
+	leaderId := ck.leaderId
+	timeoutTries := 0
+	wrongLeader := 0
+
+	for {
+		reply := GetReply{}
+		if timeoutTries > 50 {
+			// fmt.Println("KVServer.PutAppend try limit exceeded", args)
+			// return
+		}
+		if wrongLeader % len(ck.servers) == 0 &&  wrongLeader > 100 {
+			// fmt.Println("KVServer.PutAppend try limit exceeded, couldn't connect to a leader", args)
+			// return
+		}
+		// fmt.Println("KVServer.PutAppend Request to raft =>", leaderId)
+		if ok := ck.servers[leaderId].Call("KVServer.PutAppend", &args, &reply); ok {
+			switch reply.Err {
+			case OK:
+				ck.leaderId = leaderId
+				return
+			case ErrWrongLeader:
+				// fmt.Println("KVServer.PutAppend ErrWrongLeader trying again", args)
+				wrongLeader += 1
+				if wrongLeader % len(ck.servers) == 0 {
+					time.Sleep(100 * time.Millisecond)
+				}
+			case ErrTimeout:
+				// fmt.Println("KVServer.PutAppend ErrTimeout trying again", args)
+				timeoutTries += 1
+				time.Sleep(100 * time.Millisecond)
+			default:
+				fmt.Println("PutAppend Unknown", reply.Err)
+			}
+		} else {
+			// fmt.Println("KVServer.PutAppend req not ok", args)
+			time.Sleep(100 * time.Millisecond)
+			timeoutTries += 1
+		}
+		leaderId = (leaderId + 1) % len(ck.servers)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -3,10 +3,14 @@ package kvraft
 import (
 	"../labgob"
 	"../labrpc"
-	"log"
 	"../raft"
+	"fmt"
+	"log"
+	// "reflect"
+	"strconv"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 const Debug = 0
@@ -18,11 +22,43 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+func DPrintln(a ...interface{}) (n int, err error) {
+	if Debug > 0 {
+		log.Println(a...)
+	}
+	return
+}
+
+const LockUnlockDebug = false
+
+func (kv *KVServer) lock(by string) {
+	kv.mu.Lock()
+	if LockUnlockDebug {
+		fmt.Println("Lock by", by)
+	}
+}
+
+func (kv *KVServer) unlock(by string) {
+	kv.mu.Unlock()
+	if LockUnlockDebug {
+		fmt.Println("UnLock by", by)
+	}
+}
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Key      string
+	Value    string
+	Opr      string // "Put" or "Append" or "Get"
+	ReqId    int64
+	ClientId int64
+}
+
+type CallStartReply struct {
+	Err   Err
+	Value string
 }
 
 type KVServer struct {
@@ -34,16 +70,150 @@ type KVServer struct {
 
 	maxraftstate int // snapshot if log grows this big
 
+	replyMessages map[string](chan raft.ApplyMsg)
+	data          map[string]string
+	appliedToData map[string]bool
+
 	// Your definitions here.
 }
 
+func (kv *KVServer) ReqKey(ClientId, ReqId int64) string {
+	return strconv.FormatInt(ClientId, 10) + strconv.FormatInt(ReqId, 10)
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	DPrintln(kv.me, "Get received", args)
+	op := Op{
+		Opr:      GET,
+		Key:      args.Key,
+		ReqId:    args.ReqId,
+		ClientId: args.ClientId,
+	}
+
+	callStartReply := kv.CallStart("Get", op, args.Key, args.ClientId, args.ReqId)
+
+	reply.Err = callStartReply.Err
+	reply.Value = callStartReply.Value
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	DPrintln(kv.me, "PutAppend received", args)
+	op := Op{
+		Opr:      args.Op,
+		Key:      args.Key,
+		Value:    args.Value,
+		ReqId:    args.ReqId,
+		ClientId: args.ClientId,
+	}
+
+	callStartReply := kv.CallStart("PutAppend", op, args.Key, args.ClientId, args.ReqId)
+
+	reply.Err = callStartReply.Err
+}
+
+func (kv *KVServer) CallStart(method string, op Op, key string, clientId int64, reqId int64) CallStartReply {
+	reply := CallStartReply{}
+
+	_, _, isLeader := kv.rf.Start(op)
+
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return reply
+	}
+
+	replyChan := make(chan raft.ApplyMsg, 2)
+	reqKey := kv.ReqKey(clientId, reqId)
+	kv.mu.Lock()
+	kv.replyMessages[reqKey] = replyChan
+	kv.mu.Unlock()
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	for {
+		select {
+		case applyMsg := <-replyChan:
+			kv.lock(method)
+			reply.Err = OK
+
+			if method == GET {
+				reply.Value = kv.data[key]
+			}
+			DPrintln(kv.me, method, applyMsg)
+			delete(kv.replyMessages, reqKey)
+			kv.unlock(method)
+			return reply
+		case <-ticker.C:
+			// fmt.Println(method, "ApplyMsg wait ErrTimeout")
+			reply.Err = ErrTimeout
+			kv.lock(method)
+			delete(kv.replyMessages, reqKey)
+			kv.unlock(method)
+			return reply
+		}
+	}
+}
+
+func (kv *KVServer) ReceiveApplyMsg() {
+	for applyMsg := range kv.applyCh {
+		kv.lock("ReceiveApplyMsg")
+		switch op := applyMsg.Command.(type) {
+		case Op:
+			switch op.Opr {
+			case PUT, APPEND:
+				reqKey := kv.ReqKey(op.ClientId, op.ReqId)
+				kv.ApplyReqToData(applyMsg)
+				if ch, ok := kv.replyMessages[reqKey]; ok {
+					ch <- applyMsg
+				} else {
+					// fmt.Println("Channel not found in replyMessages for ", applyMsg)
+				}
+			case GET:
+				reqKey := kv.ReqKey(op.ClientId, op.ReqId)
+				if ch, ok := kv.replyMessages[reqKey]; ok {
+					ch <- applyMsg
+				} else {
+					// fmt.Println("Channel not found in replyMessages for ", applyMsg)
+				}
+			default:
+				DPrintln("Invalid op.Opr", op.Opr)
+			}
+		}
+		kv.unlock("ReceiveApplyMsg")
+	}
+}
+
+func (kv *KVServer) ApplyReqToData(applyMsg raft.ApplyMsg) {
+	switch op := applyMsg.Command.(type) {
+	case Op:
+		reqKey := kv.ReqKey(op.ClientId, op.ReqId)
+		switch op.Opr {
+		case PUT:
+			// fmt.Println(kv.me, "Putting... key => ", op.Key, "Value =>", op.Value)
+			if _, ok := kv.appliedToData[reqKey]; !ok {
+				kv.data[op.Key] = op.Value
+				kv.appliedToData[reqKey] = true
+			} else {
+				DPrintln("Request already applied", applyMsg)
+				return
+			}
+		case APPEND:
+			// fmt.Println(kv.me, "Appending... key => ", op.Key, "Value =>", op.Value)
+			if _, ok := kv.appliedToData[reqKey]; !ok {
+				prev := ""
+				if val, found := kv.data[op.Key]; found {
+					prev = val
+				}
+				kv.data[op.Key] = prev + op.Value
+				kv.appliedToData[reqKey] = true
+			} else {
+				// fmt.Println("Request already applied", applyMsg)
+				return
+			}
+		default:
+			DPrintln("Invalid op.Op", op.Opr)
+		}
+	}
+
+	// fmt.Println(kv.me, "Data =>", kv.data)
 }
 
 //
@@ -94,6 +264,11 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
+	kv.replyMessages = make(map[string](chan raft.ApplyMsg))
+	kv.data = make(map[string]string)
+	kv.appliedToData = make(map[string]bool)
+
+	go kv.ReceiveApplyMsg()
 
 	// You may need initialization code here.
 
