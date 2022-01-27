7fe01b2e7803af5cf34c8b4ed400754c5bb54547
Amit Bansal <amit.bansal@loconav.com>
2022-01-27 23:11:10 +0530

    fix in send term in appendEntries args

--- src/raft/append_entries.go
+++ src/raft/append_entries.go
@@ -27,13 +27,13 @@ func (rf *Raft) SendAppendEntries() {
 	rf.ResetRpcTimer()
 	for id := range rf.peers {
 		if id != rf.me {
-			go func(peer int) {
-				args := &AppendEntriesArgs{}
+			go func(peer int, term int, leaderId int) {
+				args := &AppendEntriesArgs{Term: term, LeaderId: leaderId}
 				reply := &AppendEntriesReply{}
 				rf.peers[peer].Call("Raft.AppendEntries", args, reply)
 
 				// DPrintf("[%d] Sending append to [%d] response", rf.me, id)
-			}(id)
+			}(id, rf.currentTerm, rf.me)
 		}
 	}
 }
